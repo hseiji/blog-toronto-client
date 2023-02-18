2f34bdebc9c8e69c49c84944a9a4cbf890f876bf
hseiji <henrique.hst@gmail.com>
2023-02-18 18:09:44 -0500

    set up aws api

--- src/config.js
+++ src/config.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-  baseURL: 'https://blog-toronto-api.onrender.com/api/',
+  // baseURL: 'https://blog-toronto-api.onrender.com/api/',
+  baseURL: 'http://3.23.64.105/api/',
 })
 
-export const PF_path = 'https://blog-toronto-api.onrender.com/images/'
+// export const PF_path = 'https://blog-toronto-api.onrender.com/images/'
+export const PF_path = 'http://3.23.64.105/images/'
