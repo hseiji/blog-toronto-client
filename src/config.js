923d65b33af50d832374feecc6f8996a25d77a03
hseiji <henrique.hst@gmail.com>
2023-03-07 20:57:39 -0500

    update baseurl and pf_path

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 export const axiosInstance = axios.create({
   // baseURL: 'https://blog-toronto-api.onrender.com/api/',
-  baseURL: 'https://henriqu3.com/api/',
+  baseURL: 'https://toronto-gems.henriqu3.com/api/',
 })
 
 // export const PF_path = 'https://blog-toronto-api.onrender.com/images/'
-export const PF_path = 'https://henriqu3.com/images/'
+export const PF_path = 'https://toronto-gems.henriqu3.com/images/'
